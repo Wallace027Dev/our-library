9a60ab83d3d0db9f2fb254a80b176077eb770b08
Wallace Vieira <wallaceofc@hotmail.com>
2024-05-29 12:01:33 -0300

    feat: adjust styles and refactor components

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,66 +1,15 @@
 import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-import { BookComponent, Container } from './styles';
-import ladraoDeRaios from '../../assets/ladrao-de-raios.jpg'
-
-interface IBook {
-  _id: number;
-  imagePath: string;
-  title: string;
-  author: string;
-  categories: { title: string }[];
-}
+import { Container, BookComponent, CategoryLabel } from './styles';
+import { Books, IBook } from '../../data/books';
+import { Genders } from '../../data/genders';
 
 interface IHomeProp {
-  searchBook: string
+  searchBook: string;
 }
-
-const Books: IBook[] = [
-  {
-    _id: 1,
-    imagePath: ladraoDeRaios,
-    title: "O ladrão de Raios",
-    author: "Rick Riordan",
-    categories: [
-      { title: "Terror" }
-    ]
-  },
-  {
-    _id: 2,
-    imagePath: ladraoDeRaios,
-    title: "Mar de Monstros",
-    author: "Rick Riordan",
-    categories: [
-      { title: "Literatura" },
-      { title: "Juvenil" },
-    ]
-  },
-  {
-    _id: 3,
-    imagePath: ladraoDeRaios,
-    title: "Maldição do Titã",
-    author: "Rick Riordan",
-    categories: [
-      { title: "Literatura" },
-      { title: "Ficção" }
-    ]
-  },
-  {
-    _id: 4,
-    imagePath: ladraoDeRaios,
-    title: "O Último Herói",
-    author: "Rick Riordan",
-    categories: [
-      { title: "Juvenil" },
-      { title: "Ficção" }
-    ]
-  },
-];
 
 const normalizeString = (str: string) => {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-};  
+};
 
 const Home: React.FC<IHomeProp> = ({ searchBook }) => {
   const [books, setBooks] = useState<IBook[]>([]);
@@ -72,51 +21,36 @@ const Home: React.FC<IHomeProp> = ({ searchBook }) => {
   }, []);
 
   useEffect(() => {
-    const normalizedSearchBook = normalizeString(searchBook)
-    const filterBooks = books.filter((book) =>{
-      const titleMatch = normalizeString(book.title).includes(normalizedSearchBook)
-      const categoryMatch = selectedCategory === 'Todos os gêneros' || book.categories.some(category => normalizeString(category.title) === normalizeString(selectedCategory));
+    const normalizedSearchBook = normalizeString(searchBook);
+    const filtered = books.filter((book) => {
+      const titleMatch = normalizeString(book.title).includes(normalizedSearchBook);
+      const categoryMatch = selectedCategory === 'Todos os gêneros' ||
+        book.categories.some(category => normalizeString(category.title) === normalizeString(selectedCategory));
       return titleMatch && categoryMatch;
     });
-      setFilteredBooks(filterBooks);
-    }, [searchBook, selectedCategory, books]);
-
-/*useEffect(() => {
-    axios.get('http://localhost:3001/books')
-      .then((response) => {
-        setBooks(response.data);
-      })
-      .catch((error) => {
-        return console.error(error)
-    })
-  }, []); */
-
-  console.log(books)
+    setFilteredBooks(filtered);
+  }, [searchBook, selectedCategory, books]);
 
   return (
     <Container>
       <aside>
         <h1>Livros por gênero</h1>
-        <button type='button' onClick={() => setSelectedCategory('Todos os gêneros')}>Todos os gêneros</button>
-        <button type='button' onClick={() => setSelectedCategory('Ficção')}>Ficção</button>
-        <button type='button' onClick={() => setSelectedCategory('Literatura')}>Literatura</button>
-        <button type='button' onClick={() => setSelectedCategory('Juvenil')}>Juvenil</button>
-        <button type='button' onClick={() => setSelectedCategory('Fantasia')}>Fantasia</button>
-        <button type='button' onClick={() => setSelectedCategory('Terror')}>Terror</button>
-        <button type='button' onClick={() => setSelectedCategory('Romance')}>Romance</button>
-        <button type='button' onClick={() => setSelectedCategory('Drama')}>Drama</button>
-        <button type='button' onClick={() => setSelectedCategory('Crônica')}>Crônica</button>
-        <button type='button' onClick={() => setSelectedCategory('Poema')}>Poema</button>
+        {Genders.map((gender) => (
+          <CategoryLabel
+            key={gender._id}
+            onClick={() => setSelectedCategory(gender.title)}
+            isSelected={selectedCategory === gender.title}
+          >
+            {gender.title}
+          </CategoryLabel>
+        ))}
       </aside>
-
       <section>
         <h1>Livros disponíveis</h1>
-
         <div>
           {filteredBooks.map((book) => (
             <BookComponent key={book._id}>
-              {/* <img src={`http://localhost:3001/uploads/${book.imagePath}`} alt={book.imagePath} /> */}
-              <img src={ book.imagePath } alt={book.imagePath} />
+              <img src={book.imagePath} alt={book.title} />
               <div>
                 <h2>{book.title}</h2>
                 <h3>{book.author}</h3>
@@ -127,7 +61,7 @@ const Home: React.FC<IHomeProp> = ({ searchBook }) => {
         </div>
       </section>
     </Container>
-  )
+  );
 }
 
 export default Home;
--- frontend/src/pages/Home/styles.ts
+++ frontend/src/pages/Home/styles.ts
@@ -1,5 +1,50 @@
 import styled from 'styled-components';
 
+export const BookComponent = styled.div`
+  display: flex;
+  min-height: 225px;
+  max-width: 460px;
+  flex-grow: 1;
+  background: #fff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+
+  img {
+    border-radius: 4px;
+    width: 148px;
+    height: 225px;
+    object-fit: cover;
+    margin: 12px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    margin-left: 24px;
+    padding: 20px;
+    width: calc(100% - 148px - 48px);
+
+    h2 {
+      font-size: 24px;
+      font-weight: 400;
+    }
+
+    h3 {
+      font-weight: 500;
+      margin: 24px 0;
+    }
+
+    h3, p {
+      font-size: 16px;
+      color: rgba(0, 0, 0, 0.6);
+    }
+  }
+`;
+
 export const Container = styled.main`
   background: #f5f6f8;
   width: 100vw;
@@ -7,7 +52,6 @@ export const Container = styled.main`
   padding: 90px 0 0 85px;
   display: flex;
   flex-direction: row;
-
 
   aside {
     margin-right: 60px;
@@ -21,26 +65,22 @@ export const Container = styled.main`
       font-weight: 700;
     }
 
-    h1, button {
+    p {
       font-size: 24px;
       margin: 12px 0;
-    }
-
-    button {
-      background: none;
-      border: none;
-    }
-
-    button:hover {
-      color: #ff971d;
       cursor: pointer;
+      color: inherit;
+      transition: color 0.3s, border-bottom 0.3s;
+
+      &:hover {
+        color: #ff971d;
+      }
     }
   }
 
   section {
     width: 100%;
     background: #fff;
-    width: 100%;
     min-height: 30vh;
     padding: 42px 75px;
 
@@ -53,40 +93,23 @@ export const Container = styled.main`
     div {
       display: flex;
       flex-wrap: wrap;
-      margin: 10px;
+      margin: -10px;
+
+      ${BookComponent} {
+        margin: 10px;
+      }
     }
-}
+  }
 `;
 
-export const BookComponent = styled.div`
-  display: flex;
-  height: 225px;
-  flex-grow: 1;
-  
-  img {
-    width: 148px;
-    height: 225px;
-  }
-  
-  div {
-    display: flex;
-    flex-direction: column;
-    margin-left: 24px;
-    width: 245px;
-  }
+export const CategoryLabel = styled.p<{ isSelected: boolean }>`
+  font-size: 24px;
+  margin: 12px 0;
+  cursor: pointer;
+  color: ${props => (props.isSelected ? '#ff971d' : 'black')};
+  transition: color 0.3s, border-bottom 0.3s;
 
-  h2 {
-    font-size: 24px;
-    font-weight: 400;
-  }
-
-  h3 {
-    font-weight: 500;
-    margin: 24px 0;
-  }
-
-  h3, p {
-    font-size: 16px;
-    color: rgba(0, 0, 0, 0.6);
+  &:hover {
+    color: #ff971d;
   }
 `;
